8ab14acb8bb91f7754441df4babac52ecbc25f79
olindenaus <madorgon@gmail.com>
2020-04-11 17:48:38 +0200

    add logout button

--- src/components/UI/Toolbar/NavigationLinks.tsx
+++ src/components/UI/Toolbar/NavigationLinks.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import * as actions from '../../../store/actions/index';
 
 export const NavigationLinks = (props: any) => {
+
+    let logButton = <NavLink to="/login">Login</NavLink>;
+
+    if (props.isAuthenticated) {
+        logButton = (
+            <>
+                <a style={{cursor: "pointer"}} onClick={props.logout}>Logout</a>
+            </>
+        )
+    }
 
     return (
         <>
             <NavLink to="/" exact>Map</NavLink>
             <NavLink to="/timekeeper">TimeTracker</NavLink>
             <NavLink to="/song">Song of a Day</NavLink>
-            <NavLink to="/login">Login</NavLink>           
+            {logButton}
         </>
     )
 };
@@ -19,4 +30,11 @@ const mapStateToProps = (state: any) => {
         isAuthenticated: state.auth.token !== null
     }
 }
-export default connect(mapStateToProps)(NavigationLinks);
+
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+        logout: () => dispatch(actions.logout())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationLinks);
--- src/store/actions/index.tsx
+++ src/store/actions/index.tsx
@@ -1,5 +1,6 @@
 export {
     auth,
+    logout,
     authCheckState
 } from './auth';
 export {
